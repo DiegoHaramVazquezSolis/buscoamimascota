329f1beea35da6c7b8d6f864fa1cadf5d185d678
DHVSGG <diego@qapla.gg>
2019-09-22 09:10:17 -0500

    Constants added

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -14,6 +14,7 @@ export const DISABLED_COLOR = '#E5E5E5';
 // Redux constants
 export const GET_LOSTED_PUBLICATION = 'GET_LOSTED_PUBLICATION';
 export const REMOVE_LOSTED_PUBLICATION = 'REMOVE_LOSTED_PUBLICATION';
+export const REMOVE_ALL_LOSTED_PUBLICATIONS = 'REMOVE_ALL_LOSTED_PUBLICATIONS';
 
 // Navigator constants
 export const MAIN_BOTTOM_NAVIGATOR = 'MainBottomNavigator';
@@ -26,5 +27,9 @@ export const LOSTED_PUBLICATIONS_LIST_SCREEN = 'LostedPublicationsListScreen';
 
 // Async storage constants
 export const ON_BOARDING_VIEWED_AS = 'on_boarding_viewed';
-export const USER_CITY_AS = 'user_city';
 export const USER_COUNTRY_AS = 'user_country';
+export const USER_REGION_AS = 'user_region';
+export const USER_CITY_AS = 'user_city';
+
+// API Key constants
+export const BATTUTA_API_KEY = 'b60ad6a578b6decda04575a54429b117';
