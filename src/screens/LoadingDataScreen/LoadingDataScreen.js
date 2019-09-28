a9c7836ba40ebaf45abd601c30b22aade5a27af0
DHVSGG <diego@qapla.gg>
2019-09-28 10:18:37 -0500

    Detect initial link added

--- src/screens/LoadingDataScreen/LoadingDataScreen.js
+++ src/screens/LoadingDataScreen/LoadingDataScreen.js
@@ -3,6 +3,7 @@ import Orientation from 'react-native-orientation-locker';
 
 import { getAsyncStorageData } from './../../utils/LocalStorage';
 import { ON_BOARDING_SCREEN, MAIN_BOTTOM_NAVIGATOR, ON_BOARDING_VIEWED_AS } from '../../utils/Constants';
+import { firebaseLinks } from '../../services/firebase';
 
 const LoadingDataScreen = ({ navigation }) => {
 
@@ -19,6 +20,11 @@ const LoadingDataScreen = ({ navigation }) => {
              */
             if(await getAsyncStorageData(ON_BOARDING_VIEWED_AS) === 'true'){
                 Orientation.lockToPortrait();
+
+                const initialLink = await firebaseLinks.getInitialLink();
+                if (initialLink) {
+                    console.log('Initial link:', initialLink);
+                }
                 navigation.navigate(MAIN_BOTTOM_NAVIGATOR);
             } else {
                 navigation.navigate(ON_BOARDING_SCREEN);
