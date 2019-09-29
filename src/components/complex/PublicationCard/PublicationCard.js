1c8ed6559e901e428525778d692053d599ba5dac
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2019-09-28 23:50:10 -0500

    Icons updated to accept custom color

--- src/components/complex/PublicationCard/PublicationCard.js
+++ src/components/complex/PublicationCard/PublicationCard.js
@@ -6,35 +6,42 @@ import GlobalStyles from '../../../utils/GlobalStyles';
 import TextButton from '../../simple/TextButton/TextButton';
 import Assets from '../../../../assets/Assets';
 import { returnTextBasedOnMaxLengthWithLimit } from '../../../utils/Utils';
+import { PRIMARY_COLOR } from '../../../utils/Constants';
 
-const PublicationCard = ({ id = '', image = '', name = '', description = '', onSharePress = () => {}, lastChild = false }) => {
+const PublicationCard = ({ onPublicationPress = () => {}, id = '', image = '', name = '', description = '', onSharePress = () => {}, lastChild = false }) => {
     return (
-        <View style={[GlobalStyles.alignItemsCenter, styles.card, { marginBottom: lastChild ? 12 : 0}]}>
-            <View>
-                <Image
-                    style={styles.image}
-                    source={{ uri: image }} />
-            </View>
-            <View style={styles.interactionContainer}>
+        <TouchableWithoutFeedback onPress={() => onPublicationPress(id)}>
+            <View style={[GlobalStyles.alignItemsCenter, styles.card, { marginBottom: lastChild ? 12 : 0}]}>
                 <View>
-                    <Text style={styles.name}>
-                        {returnTextBasedOnMaxLengthWithLimit(name, 18, 15)}
-                    </Text>
-                    <Text style={styles.description}>
-                        {returnTextBasedOnMaxLengthWithLimit(description, 60, 56)}
-                    </Text>
+                    <Image
+                        style={styles.image}
+                        source={{ uri: image }} />
                 </View>
-                <View style={styles.actionsContainer}>
-                    <TextButton>Contactar</TextButton>
-                    <View style={styles.iconsContainer}>
-                        <TouchableWithoutFeedback onPress={() => onSharePress(id)}>
-                            <Assets.svg.ShareIcon style={styles.shareIcon} />
-                        </TouchableWithoutFeedback>
-                        <Assets.svg.MoreOptionsIcon style={styles.optionIcon} />
+                <View style={styles.interactionContainer}>
+                    <View>
+                        <Text style={styles.name}>
+                            {returnTextBasedOnMaxLengthWithLimit(name, 18, 15)}
+                        </Text>
+                        <Text style={styles.description}>
+                            {returnTextBasedOnMaxLengthWithLimit(description, 60, 56)}
+                        </Text>
+                    </View>
+                    <View style={styles.actionsContainer}>
+                        <TextButton>Contactar</TextButton>
+                        <View style={styles.iconsContainer}>
+                            <TouchableWithoutFeedback onPress={() => onSharePress(id)}>
+                                <Assets.svg.ShareIcon
+                                    style={styles.shareIcon}
+                                    fill={PRIMARY_COLOR} />
+                            </TouchableWithoutFeedback>
+                            <Assets.svg.MoreOptionsIcon
+                                style={styles.optionIcon}
+                                fill={PRIMARY_COLOR} />
+                        </View>
                     </View>
                 </View>
             </View>
-        </View>
+        </TouchableWithoutFeedback>
     );
 };
 
--- src/components/complex/PublicationsList/PublicationList.js
+++ src/components/complex/PublicationsList/PublicationList.js
@@ -1,11 +1,22 @@
-import React, { useReducer } from 'react';
+import React from 'react';
 import { Share, View, FlatList } from 'react-native';
+import { withNavigation } from 'react-navigation';
 
 import styles from './styles';
 import PublicationCard from '../PublicationCard/PublicationCard';
 import { createDynamicLink } from '../../../services/dynamicLinks';
+import { PUBLICATION_DETAILS_SCREEN } from '../../../utils/Constants';
 
-const PublicationList = ({ publications }) => {
+const PublicationList = ({ navigation = {}, publications = {} }) => {
+
+    /**
+     * Send the user to publication details screen and send the related
+     * params of the selected pet
+      @param {string} id Identifier of the publication on the database
+     */
+    onPublicationPress = (id) => {
+        navigation.navigate(PUBLICATION_DETAILS_SCREEN, { ...publications[id] });
+    }
 
     /**
      * Set the selected pet and open the share publication dialog
@@ -30,10 +41,11 @@ const PublicationList = ({ publications }) => {
                         key={item.id}
                         {...item}
                         onSharePress={onSharePress}
+                        onPublicationPress={onPublicationPress}
                         lastChild={index + 1 === Object.keys(publications).length} />
                 )} />
         </View>
     );
 }
 
-export default PublicationList;
+export default withNavigation(PublicationList);
