e7b3a56dd88319ef08146834755667ff59eb6b86
DHVSGG <diego@qapla.gg>
2019-10-19 18:09:49 -0500

    Get and save notificationId implemented

--- src/redux/actions/UserActions.js
+++ src/redux/actions/UserActions.js
@@ -1,6 +1,9 @@
+import OneSignal from 'react-native-onesignal';
+
 import { USER_LOGGED, USER_NOT_LOGGED } from '../../utils/Constants';
 
 import { auth } from '../../services/firebase';
+import { updateUserInfo } from '../../services/database';
 
 /**
  * Action to call when the user is logged on the app
@@ -19,9 +22,12 @@ const userIsNotLogged = () => ({ type: USER_NOT_LOGGED });
 export const checkIfUserIsLogged = () => (dispatch) => {
     auth.onAuthStateChanged((user) => {
         if (user) {
+            OneSignal.addEventListener('ids', (device) => {
+                updateUserInfo(user.uid, { notificationId: device.userId });
+            });
+            
             return dispatch(userIsLogged());
         }
         return dispatch(userIsNotLogged());
     });
 }
-
--- src/services/database.js
+++ src/services/database.js
@@ -1,3 +1,10 @@
-import { firestoreDatabase } from './firebase';
+import { firestoreDatabase, realTimeDatabase } from './firebase';
 
 export const lostedRef = firestoreDatabase.collection('Lost');
+export const usersRef = realTimeDatabase.ref('/Users');
+
+export const updateUserInfo = (uid, userInfo) => {
+    
+
+    return usersRef.child(uid).update(userInfo);
+}
