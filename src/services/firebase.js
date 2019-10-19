4294ac66d91e4e6db577dd50701f3ce393f22e0a
DHVSGG <diego@qapla.gg>
2019-10-19 18:08:49 -0500

    RealTime database added

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,8 +1,11 @@
 import firebase from '@react-native-firebase/app';
 import '@react-native-firebase/firestore';
+import '@react-native-firebase/database';
 import '@react-native-firebase/dynamic-links';
 import '@react-native-firebase/auth';
+
 export const firestoreDatabase = firebase.firestore();
+export const realTimeDatabase = firebase.database();
 export const firebaseDynamicLinksGenerator = firebase.dynamicLinks();
 export const auth = firebase.auth();
 
