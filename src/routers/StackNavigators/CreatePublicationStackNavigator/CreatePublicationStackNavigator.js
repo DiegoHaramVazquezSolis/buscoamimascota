522bb570caf7e63188aa6256d80af1fea4f37fc0
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2019-10-30 20:20:08 -0600

    CreatePublicationStackNavigator updated

--- src/routers/StackNavigators/CreatePublicationStackNavigator/CreatePublicationStackNavigator.js
+++ src/routers/StackNavigators/CreatePublicationStackNavigator/CreatePublicationStackNavigator.js
@@ -1,13 +1,21 @@
 import createNativeStackNavigator from 'react-native-screens/createNativeStackNavigator';
 
-import { CHOOSE_PUBLICATION_TYPE_SCREEN } from '../../../utils/Constants';
+import { CHOOSE_PUBLICATION_TYPE_SCREEN, PET_INFO_FORM_SCREEN, PET_INFO_LOCATION_SCREEN } from '../../../utils/Constants';
 
 import ChoosePublicationTypeScreen from '../../../screens/ChoosePublicationTypeScreen/ChoosePublicationTypeScreen';
+import PetInfoFormScreen from '../../../screens/PetInfoFormScreen/PetInfoFormScreen';
+import PetInfoLocationScreen from '../../../screens/PetInfoLocationScreen/PetInfoLocationScreen';
 
 const routesConfig = {
     [CHOOSE_PUBLICATION_TYPE_SCREEN]: {
         screen: ChoosePublicationTypeScreen
-    }
+    },
+    [PET_INFO_FORM_SCREEN]: {
+        screen: PetInfoFormScreen
+    },
+    [PET_INFO_LOCATION_SCREEN]: {
+        screen: PetInfoLocationScreen
+    },
 };
 
 export default CreatePublicationStackNavigator = createNativeStackNavigator(routesConfig);
