47c6aa348684b242e104d75409c7dcf75379cd6e
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2019-11-03 22:34:17 -0600

    PetInfoImageScreen added to stack

--- src/routers/StackNavigators/CreatePublicationStackNavigator/CreatePublicationStackNavigator.js
+++ src/routers/StackNavigators/CreatePublicationStackNavigator/CreatePublicationStackNavigator.js
@@ -1,11 +1,12 @@
 import createNativeStackNavigator from 'react-native-screens/createNativeStackNavigator';
 
-import { CHOOSE_PUBLICATION_TYPE_SCREEN, PET_INFO_FORM_SCREEN, PET_INFO_LOCATION_SCREEN, PET_INFO_CONTACT_SCREEN } from '../../../utils/Constants';
+import { CHOOSE_PUBLICATION_TYPE_SCREEN, PET_INFO_FORM_SCREEN, PET_INFO_LOCATION_SCREEN, PET_INFO_CONTACT_SCREEN, PET_INFO_IMAGE_SCREEN } from '../../../utils/Constants';
 
 import ChoosePublicationTypeScreen from '../../../screens/ChoosePublicationTypeScreen/ChoosePublicationTypeScreen';
 import PetInfoFormScreen from '../../../screens/PetInfoFormScreen/PetInfoFormScreen';
 import PetInfoLocationScreen from '../../../screens/PetInfoLocationScreen/PetInfoLocationScreen';
 import PetInfoContactScreen from '../../../screens/PetInfoContactScreen/PetInfoContactScreen';
+import PetInfoImageScreen from '../../../screens/PetInfoImageScreen/PetInfoImageScreen';
 
 const routesConfig = {
     [CHOOSE_PUBLICATION_TYPE_SCREEN]: {
@@ -19,6 +20,9 @@ const routesConfig = {
     },
     [PET_INFO_CONTACT_SCREEN]: {
         screen: PetInfoContactScreen
+    },
+    [PET_INFO_IMAGE_SCREEN]: {
+        screen: PetInfoImageScreen
     }
 };
 
