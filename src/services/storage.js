602ecf4779e3874092a4828e107539baca863c87
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2019-11-03 22:32:45 -0600

    Utils updated

--- src/services/storage.js
+++ src/services/storage.js
@@ -4,11 +4,15 @@ export const lostedStorageRef = storage.ref('/Lost');
 export const adoptionStorageRef = storage.ref('/Adoption');
 
 export async function uploadPetImage(losted, publicationId, image) {
-    if (losted) {
-        await lostedStorageRef.child(publicationId).putString(image, 'base64');
-        return await lostedStorageRef.child(publicationId).getDownloadURL();
-    } else {
-        await adoptionStorageRef.child(publicationId).putString(image, 'base64');
-        return await adoptionStorageRef.child(publicationId).getDownloadURL();
+    try {
+        if (losted) {
+            await lostedStorageRef.child(publicationId).putString(image, 'base64');
+            return await lostedStorageRef.child(publicationId).getDownloadURL();
+        } else {
+            await adoptionStorageRef.child(publicationId).putString(image, 'base64');
+            return await adoptionStorageRef.child(publicationId).getDownloadURL();
+        }
+    } catch (error) {
+        console.error(error);
     }
 }
--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,3 +1,5 @@
+import { firestoreValues } from '../services/firebase';
+
 /**
  * @description Truncate a string if the length of the string is greater or equal than maxLength
  * @param {string} text Text to evaluate
@@ -6,7 +8,7 @@
  */
 export function returnTextBasedOnMaxLength (text, maxLength) {
     if (maxLength === 0 || text.length < maxLength) {
-        
+
         return text;
     }
 
@@ -14,7 +16,7 @@ export function returnTextBasedOnMaxLength (text, maxLength) {
 }
 
 /**
- * @description Truncate a string to the given limit size if the length of the string is 
+ * @description Truncate a string to the given limit size if the length of the string is
  * greater or equal than maxLength
  * @param {string} text Text to evaluate
  * @param {number} maxLength Max length allowed on the text
@@ -22,10 +24,62 @@ export function returnTextBasedOnMaxLength (text, maxLength) {
  * @example returnTextBasedOnMaxLengthWithLimit('Hello', 5, 2) return 'He'
  */
 export function returnTextBasedOnMaxLengthWithLimit (text, maxLength, limit) {
-    if (maxLength === 0 || text.length < maxLength) {
-
-        return text;
+    const textHaveEndLine = /(\r\n|\n|\r)/gm.exec(text);
+    if (textHaveEndLine) {
+        if (textHaveEndLine.index <= limit) {
+        return text.substring(0, textHaveEndLine.index);
+        }
     }
 
-    return `${text.substring(0, limit)}...`;
+    if (maxLength === 0 || text.length < maxLength) {
+
+        return text.replace(/(\r\n|\n|\r)/gm," ");
+    }
+
+    return `${text.replace(/(\r\n|\n|\r)/gm,"").substring(0, limit)}...`;
+}
+
+/**
+ * Build a GeoPoint to store it on the firestore database (only apply to firestore
+ * don't use on real time database)
+ * @param {number} latitude Latitude of the location
+ * @param {number} longitude Longitude of the location
+ */
+export function createFirestoreGeoPoint({ latitude, longitude }) {
+    return new firestoreValues.GeoPoint(latitude, longitude);
+}
+
+/**
+ * Create a TimeStamp value to store it on the firestore database (only apply to firestore
+ * don't use on real time database)
+ * @param {number} timeStampMilliseconds TimeStamp to save (in milliseconds)
+ */
+export function createFirestoreTimeStamp(timeStampMilliseconds) {
+    return new firestoreValues.Timestamp(timeStampMilliseconds / 1000, 0);
+}
+
+/**
+ * Get the current UTC timeStamp
+ * @returns {Date}
+ */
+export function getCurrentUTCDate() {
+    const localDate = new Date();
+
+    return new Date(
+        localDate.getUTCFullYear(),
+        localDate.getUTCMonth(),
+        localDate.getUTCDate(),
+        localDate.getUTCHours(),
+        localDate.getUTCMinutes(),
+        localDate.getUTCSeconds(),
+        localDate.getUTCMilliseconds()
+    );
+}
+
+/**
+ * Convert an UTC Date to the user's local date
+ * @param {Date} UTCDate Date UTC
+ */
+export function convertUTCDateToLocalDate(UTCDate) {
+    return new Date(UTCDate.getTime() - UTCDate.getTimezoneOffset() * 60 * 1000);
 }
