0a603163373adcf2f97f457579924a4ba0d97383
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2020-02-15 15:10:31 -0600

    Redux store exported as an instance

--- App.js
+++ App.js
@@ -7,7 +7,8 @@ import GlobalStyles from './src/utils/GlobalStyles';
 
 import { PRIMARY_COLOR } from './src/utils/Constants';
 
-import { configureStore } from './src/redux/configureStore';
+import store, { configureStore } from './src/redux/configureStore';
+import { setCurrentScreen, setPreviousScreen } from './src/redux/actions/ScreensActions';
 
 import Grid from './src/components/simple/Grid/Grid';
 import Router from './src/routers/Router';
@@ -15,13 +16,40 @@ import Router from './src/routers/Router';
 console.disableYellowBox = true;
 
 useScreens();
+configureStore();
+
+/**
+ * @description Returns the current screen from navigation state
+ *
+ * @param {Object} navigationState Navigation state object
+ */
+function getCurrentRouteName(navigationState) {
+    if (!navigationState) {
+      return null;
+    }
+    const route = navigationState.routes[navigationState.index];
+
+    if (route.routes) {
+        return getCurrentRouteName(route);
+    }
+
+    return route.routeName;
+}
 
 const App = () => (
-    <Provider store={configureStore()}>
+    <Provider store={store}>
         <View style={[GlobalStyles.flex1, { backgroundColor: '#EDF0F2' }]}>
             <Grid show={false} />
             <StatusBar backgroundColor={PRIMARY_COLOR} />
-            <Router />
+            <Router onNavigationStateChange={(prevState, currentState) => {
+                const currentScreen = getCurrentRouteName(currentState);
+                const previousScreen = getCurrentRouteName(prevState);
+
+                if (currentScreen !== previousScreen) {
+                    setCurrentScreen(currentScreen)(store.dispatch);
+                    setPreviousScreen(previousScreen)(store.dispatch);
+                }
+            }} />
         </View>
     </Provider>
 );
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -6,15 +6,17 @@ import { getLostedPublications } from './actions/LostedPublicationsActions';
 import { getAdoptionPublications } from './actions/AdoptionPublicationsActions';
 import { checkIfUserIsLogged } from './actions/UserActions';
 
+const store = createStore(RootReducer, applyMiddleware(thunkMiddleware));
 
 /**
  * Create the store and load important data at the begining of the app
  */
 export function configureStore() {
-    const store = createStore(RootReducer, applyMiddleware(thunkMiddleware));
     store.dispatch(getLostedPublications());
     store.dispatch(getAdoptionPublications());
     store.dispatch(checkIfUserIsLogged());
 
     return store;
 }
+
+export default store;
