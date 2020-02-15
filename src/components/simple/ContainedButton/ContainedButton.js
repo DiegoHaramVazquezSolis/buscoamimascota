95baa180eb09e91ca779e7e7da71ec7e601a85cc
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2020-02-15 15:14:07 -0600

    Buttons props added

--- src/components/simple/ContainedButton/ContainedButton.js
+++ src/components/simple/ContainedButton/ContainedButton.js
@@ -4,9 +4,9 @@ import { TouchableOpacity, View, Text } from 'react-native';
 import GlobalStyles from '../../../utils/GlobalStyles';
 import styles from './styles';
 
-const ContainedButton = ({ children = '', onPress = () => {}, size = '' }) => (
+const ContainedButton = ({ children = '', onPress = () => {}, size = '', style = {} }) => (
     <TouchableOpacity
-        style={[ GlobalStyles.backgroundColorPrimary, size === 'sm' ? styles.containerSm : styles.containerMd ]}
+        style={[ GlobalStyles.backgroundColorPrimary, size === 'sm' ? styles.containerSm : styles.containerMd, style]}
         onPress={onPress}
         activeOpacity={.6}>
         <Text style={size === 'sm' ? styles.contentSm : styles.contentMd}>{children}</Text>
--- src/components/simple/TextButton/TextButton.js
+++ src/components/simple/TextButton/TextButton.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { TouchableHighlight, Text } from 'react-native';
 import styles from './styles';
 
-const TextButton = ({ onPress = () => {}, children = '' }) => {
+const TextButton = ({ onPress = () => {}, children = '', style = {} }) => {
     return (
-        <TouchableHighlight underlayColor='rgba(0, 0, 0, .1)' onPress={onPress}>
+        <TouchableHighlight underlayColor='rgba(0, 0, 0, .1)' onPress={onPress} style={style}>
             <Text style={styles.textButton}>{children}</Text>
         </TouchableHighlight>
     );
